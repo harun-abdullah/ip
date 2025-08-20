9c2f6adaf192e7a50129f68446705f54f2a62574
Harun <theayyyron@gmail.com>
2025-08-20 16:22:58 +0800

    Added greeting function

--- src/main/java/Rakan.java
+++ src/main/java/Rakan.java
@@ -1,11 +1,10 @@
 public class Rakan {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Wazzap. I'm Rakan\n How can I help you?");
     }
 
 
