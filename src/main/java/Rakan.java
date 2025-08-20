8caeae773c9f201a65fcce28f9c96e99b7944ec8
Harun <theayyyron@gmail.com>
2025-08-20 16:32:57 +0800

    Added exit function

--- src/main/java/Rakan.java
+++ src/main/java/Rakan.java
@@ -1,10 +1,15 @@
 public class Rakan {
     public static void main(String[] args) {
         greet();
+        exit();
     }
 
     public static void greet() {
-        System.out.println("Wazzap. I'm Rakan\n How can I help you?");
+        System.out.println("Wazzap. I'm Rakan \uD83D\uDD25 \uD83D\uDD25 \uD83D\uDD25 \nHow can I help you?");
+    }
+
+    public static void exit() {
+        System.out.println("Oh, bye then! See you later vro \uD83E\uDD40 \uD83E\uDD40 \uD83E\uDD40 ");
     }
 
 
