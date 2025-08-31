cd637ab6cd0e6f50f7e7323d248cdf83fce98d30
Harun <theayyyron@gmail.com>
2025-08-31 17:03:58 +0800

    Create ParsedEvent class

--- src/main/java/ParsedEvent.java
+++ src/main/java/ParsedEvent.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
 
 public class ParsedEvent {
+    private final String description;
+    private final LocalDateTime from;
+    private final LocalDateTime to;
+
+    public ParsedEvent(String description, LocalDateTime from, LocalDateTime to) {
+        this.description = description;
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
 }
+
