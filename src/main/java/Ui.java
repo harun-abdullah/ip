4424a04b41d24f15d522d0f0de52d3ad93e2c7f7
Harun <theayyyron@gmail.com>
2025-08-31 13:46:22 +0800

    Add showList method to UI class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Ui {
 
     public void entry(String message) {
@@ -12,6 +14,22 @@ public class Ui {
 
     public void exit() {
         entry("Oh, bye then! See you later vro \uD83E\uDD40 \uD83E\uDD40 \uD83E\uDD40");
+    }
+
+    public void showList(ArrayList<Task> taskList) throws RakanException {
+        if (taskList.isEmpty()) {
+            throw new RakanException("Nothing here yet!");
+        } else {
+            StringBuilder list = new StringBuilder("Tasklist:");
+            final int[] index = {1}; // use array to mutate inside lambda
+
+            taskList.forEach(task -> {
+                list.append("\n").append(index[0]).append(". ").append(task);
+                index[0]++;
+            });
+
+            entry(list.toString());
+        }
     }
 
 }
