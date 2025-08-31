86ffeb18f5880acfec4c60c914570ba78dcedac3
Harun <theayyyron@gmail.com>
2025-08-31 17:03:33 +0800

    Create ParsedDeadline class

--- src/main/java/ParsedDeadline.java
+++ src/main/java/ParsedDeadline.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
 
 public class ParsedDeadline {
+    private final String description;
+    private final LocalDateTime by;
+
+    public ParsedDeadline(String description, LocalDateTime by) {
+        this.description = description;
+        this.by = by;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getBy() {
+        return by;
+    }
 }
+
