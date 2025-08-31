6e63f4d2ab0cb4c5964529d1286e0a0095f53f51
Harun <theayyyron@gmail.com>
2025-08-31 13:41:52 +0800

    Add task marking  method to TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -118,4 +118,48 @@ public class TaskList {
         }
     }
 
+    public void handleMark(String input, boolean isMark, Ui ui) throws RakanException, IOException
+    {
+        String keyword = isMark ? "mark" : "unmark";
+        String[] parts = input.split("\\s+", 2);
+
+        if (parts.length < 2) {
+            throw new RakanException("Please provide a task number after \"" + keyword + "\".");
+        }
+
+        try {
+            int taskNumber = Integer.parseInt(parts[1]);
+            int index = taskNumber - 1;
+
+            // check if number provided is valid
+            if (index < 0 || index >= tasks.size() || isNull(tasks.get(index))) {
+                throw new RakanException("No such task: " + taskNumber);
+            }
+
+            Task task = tasks.get(index);
+
+            if (isMark) {
+                if (task.isDone) {
+                    throw new RakanException("This task is already marked as done!");
+                }
+                task.markAsDone();
+            } else {
+                if (!task.isDone) {
+                    throw new RakanException("This task is already marked as not done!");
+                }
+                task.markAsNotDone();
+            }
+
+            Storage.saveTasks(tasks);
+
+            ui.entry((isMark
+                    ? "Nice! I've marked this task as done:"
+                    : "OK, I've marked this task as not done yet:")
+                    + "\n" + task
+            );
+        } catch (NumberFormatException e) {
+            throw new RakanException("Invalid task number: " + parts[1]);
+        }
+    }
+
 }
