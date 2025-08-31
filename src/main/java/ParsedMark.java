ad3c5cd098e52d33e576b85175cc9c148a1b830f
Harun <theayyyron@gmail.com>
2025-08-31 18:10:56 +0800

    Create ParsedMark class

---
+++ src/main/java/ParsedMark.java
@@ -0,0 +1,18 @@
+public class ParsedMark {
+    private final int taskIndex;
+    private final boolean isMark;
+
+    public ParsedMark(int taskIndex, boolean isMark) {
+        this.taskIndex = taskIndex;
+        this.isMark = isMark;
+    }
+
+    public int getTaskIndex() {
+        return taskIndex;
+    }
+
+    public boolean isMark() {
+        return isMark;
+    }
+}
+
