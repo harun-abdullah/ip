7fe3287bfdc0541fd7ac3f96a69fd51f7514ce0c
Harun <theayyyron@gmail.com>
2025-08-31 12:20:24 +0800

    Create Ui class with some methods

---
+++ src/main/java/Ui.java
@@ -0,0 +1,17 @@
+public class Ui {
+
+    public void entry(String message) {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println(message);
+        System.out.println("---------------------------------------------------------------------------------");
+    }
+
+    public void greet() {
+        entry("Wazzap. I'm Rakan \uD83D\uDD25 \uD83D\uDD25 \uD83D\uDD25\nHow can I help you?");
+    }
+
+    public void exit() {
+        entry("Oh, bye then! See you later vro \uD83E\uDD40 \uD83E\uDD40 \uD83E\uDD40");
+    }
+
+}
