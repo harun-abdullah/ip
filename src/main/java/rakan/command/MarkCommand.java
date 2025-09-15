5acfb582ae7c19b14c8843d3817f74471882caf5
Harun <theayyyron@gmail.com>
2025-09-15 22:40:47 +0800

    Add MarkCommand to handle task marking

---
+++ src/main/java/rakan/command/MarkCommand.java
@@ -0,0 +1,33 @@
+package rakan.command;
+
+import rakan.RakanException;
+import rakan.parser.ParsedMark;
+import rakan.storage.Storage;
+import rakan.tasklist.TaskList;
+import rakan.ui.Ui;
+
+public class MarkCommand extends Command {
+    private String input;
+
+    /**
+     * Creates a mark command from the user's input.
+     *
+     * @param input the full command string like "mark 3"
+     */
+    public MarkCommand(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws RakanException {
+        String[] split = input.split(" ");
+        int taskNum = Integer.parseInt(split[1]);
+        ParsedMark parsedMark = new ParsedMark(taskNum, true);
+        tasks.handleMark(parsedMark);
+        ui.showMessages(
+                " Nice! I've marked this task as done:",
+                "   " + tasks.getTasks().get(taskNum - 1)
+        );
+        storage.saveTasks(tasks.getTasks());
+    }
+}
