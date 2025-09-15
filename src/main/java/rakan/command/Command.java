5e4a97aa34de8817bd5a2af00deba18246ce378c
Harun <theayyyron@gmail.com>
2025-09-15 18:56:02 +0800

    Create abstract Command class
    Abstract base class for Command classes for different
    commands to be used in Rakan.

---
+++ src/main/java/rakan/command/Command.java
@@ -0,0 +1,21 @@
+package rakan.command;
+
+import java.io.IOException;
+
+import rakan.RakanException;
+import rakan.storage.Storage;
+import rakan.tasklist.TaskList;
+import rakan.ui.Ui;
+
+public abstract class Command {
+
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws RakanException;
+
+    public boolean isExit() {
+        return false;
+    }
+
+    protected void saveTasksToFile(TaskList tasks, Ui ui, Storage storage) {
+        storage.saveTasks(tasks.getTasks());
+    }
+}
