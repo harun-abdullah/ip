04c3cc965d39ba4f5ff9c083224fbeb4e6d5d304
Harun <theayyyron@gmail.com>
2025-09-16 00:01:04 +0800

    Revamp getResponse() with new Ui and Parser

--- src/main/java/rakan/Rakan.java
+++ src/main/java/rakan/Rakan.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import rakan.command.Command;
 import rakan.parser.ParsedDeadline;
 import rakan.parser.ParsedEvent;
 import rakan.parser.ParsedMark;
@@ -22,6 +23,7 @@ public class Rakan {
     private Ui ui;
     private TaskList taskList;
     private Storage storage;
+    private boolean shouldExit = false;
 
     /**
      * Constructs Rakan instance.
@@ -43,60 +45,22 @@ public class Rakan {
      */
     public String getResponse(String userInput) {
         try {
-            if (userInput.equalsIgnoreCase("bye")) {
-                return ui.exit();
-            } else if (userInput.equalsIgnoreCase("list")) {
-                return ui.showList(taskList.getTasks());
-            } else if (userInput.toLowerCase().startsWith("mark")) {
-                ParsedMark parsedMark = Parser.parseMark(userInput, true, taskList.getTasks().size());
-                Task task = taskList.getTasks().get(parsedMark.getTaskIndex());
-                taskList.handleMark(parsedMark);
-                storage.saveTasks(taskList.getTasks());
-                return "Nice! I've marked this task as done:\n" + task;
-            } else if (userInput.toLowerCase().startsWith("unmark")) {
-                ParsedMark parsedMark = Parser.parseMark(userInput, false, taskList.getTasks().size());
-                Task task = taskList.getTasks().get(parsedMark.getTaskIndex());
-                taskList.handleMark(parsedMark);
-                storage.saveTasks(taskList.getTasks());
-                return "OK, I've marked this task as not done yet:\n" + task;
-            } else if (userInput.toLowerCase().startsWith("delete")) {
-                int index = Parser.parseDelete(userInput);
-                Task task = taskList.getTasks().get(index);
-                taskList.handleDelete(index);
-                storage.saveTasks(taskList.getTasks());
-                return "Yes boss. I've removed the task below:\n" + task +
-                        "\nNow you have " + taskList.getTasks().size() + " tasks in the list";
-            } else if (userInput.toLowerCase().startsWith("todo")) {
-                String description = Parser.parseTodo(userInput);
-                ToDo toDo = new ToDo(description);
-                taskList.addTask(toDo);
-                storage.saveTasks(taskList.getTasks());
-                return "Got it. I've added this task:\n  " + toDo +
-                        "\nNow you have " + taskList.getTasks().size() + " tasks.";
-            } else if (userInput.toLowerCase().startsWith("deadline")) {
-                ParsedDeadline parsedDeadline = Parser.parseDeadline(userInput);
-                DeadLine deadline = new DeadLine(parsedDeadline.getDescription(), parsedDeadline.getBy());
-                taskList.addTask(deadline);
-                storage.saveTasks(taskList.getTasks());
-                return "Got it. I've added this task:\n  " + deadline +
-                        "\nNow you have " + taskList.getTasks().size() + " tasks.";
-            } else if (userInput.toLowerCase().startsWith("event")) {
-                ParsedEvent parsedEvent = Parser.parseEvent(userInput);
-                Event event = new Event(parsedEvent.getDescription(), parsedEvent.getFrom(), parsedEvent.getTo());
-                taskList.addTask(event);
-                storage.saveTasks(taskList.getTasks());
-                return "Got it. I've added this task:\n  " + event +
-                        "\nNow you have " + taskList.getTasks().size() + " tasks.";
-            } else if (userInput.toLowerCase().startsWith("find")) {
-                String keyword = Parser.parseFind(userInput);
-                ArrayList<Task> results = taskList.find(keyword);
-                return ui.showFindResults(results);
-            } else {
-                throw new RakanException("Sorry, not sure what that means");
-            }
-        } catch (RakanException | IOException e) {
+            ui.clearMessages();
+            Command c = Parser.parse(userInput);
+
+            c.execute(taskList, ui, storage);
+
+            shouldExit = c.isExit();
+
+            return ui.getResponse();
+
+        } catch (RakanException e) {
             return e.getMessage();
         }
+    }
+
+    public boolean shouldExit() {
+        return shouldExit;
     }
 
     public Ui getUi() {
