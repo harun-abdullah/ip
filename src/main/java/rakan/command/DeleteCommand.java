b04bc2141941e06ad371ca54f7e201fe7082b88f
Harun <theayyyron@gmail.com>
2025-09-16 01:06:06 +0800

    Add index validation for delete command

--- src/main/java/rakan/command/DeleteCommand.java
+++ src/main/java/rakan/command/DeleteCommand.java
@@ -3,6 +3,7 @@ package rakan.command;
 import java.io.IOException;
 
 import rakan.RakanException;
+import rakan.parser.Parser;
 import rakan.storage.Storage;
 import rakan.task.Task;
 import rakan.tasklist.TaskList;
@@ -18,8 +19,8 @@ public class DeleteCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws RakanException {
 
-        String[] deleteParts = input.split(" ");
-        int index = Integer.parseInt(deleteParts[1]);
+        String[] split = input.split(" ");
+        int index = Parser.validateTaskNumber(split[1], tasks.getTasks().size()) - 1;
         Task task = tasks.getTasks().get(index);
         try {
             tasks.handleDelete(index);
