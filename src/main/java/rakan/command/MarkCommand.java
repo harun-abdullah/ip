0c777845d0bab8e8c46847ec208bd8070a3ad699
Harun <theayyyron@gmail.com>
2025-09-20 21:12:08 +0800

    Add JavaDoc for MarkCommand and UnmarkCommand

--- src/main/java/rakan/command/MarkCommand.java
+++ src/main/java/rakan/command/MarkCommand.java
@@ -7,18 +7,33 @@ import rakan.storage.Storage;
 import rakan.tasklist.TaskList;
 import rakan.ui.Ui;
 
+/**
+ * In charge of marking task at given index, and saving the change to storage.
+ */
 public class MarkCommand extends Command {
+    /**
+     * String containing index of task to be marked in tasklist.
+     */
     private String input;
 
     /**
-     * Creates a mark command from the user's input.
+     * Constructor for MarkCommand.
      *
-     * @param input the full command string like "mark 3"
+     * @param input User input containing index of task to mark.
      */
     public MarkCommand(String input) {
         this.input = input;
     }
 
+    /**
+     * Marks given task as done.
+     * Displays Ui message afterward and saves changes to storage.
+     *
+     * @param tasks TaskList to work with.
+     * @param ui Ui object to display messages during execution.
+     * @param storage Storage object used during task saving.
+     * @throws RakanException Exception when task is already done.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws RakanException {
         String[] split = input.split(" ");
--- src/main/java/rakan/command/UnmarkCommand.java
+++ src/main/java/rakan/command/UnmarkCommand.java
@@ -7,13 +7,33 @@ import rakan.storage.Storage;
 import rakan.tasklist.TaskList;
 import rakan.ui.Ui;
 
+/**
+ * In charge of marking task at given index, and saving the change to storage.
+ */
 public class UnmarkCommand extends Command {
+    /**
+     * String containing index of task to be marked in tasklist.
+     */
     private String input;
 
+    /**
+     * Constructor for UnmarkCommand.
+     *
+     * @param input User input containing index of task to unmark.
+     */
     public UnmarkCommand(String input) {
         this.input = input;
     }
 
+    /**
+     * Marks given task as not done.
+     * Displays Ui message afterward and saves changes to storage.
+     *
+     * @param tasks TaskList to work with.
+     * @param ui Ui object to display messages during execution.
+     * @param storage Storage object used during task saving.
+     * @throws RakanException Exception when task is already done.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws RakanException {
         String[] split = input.split(" ");
